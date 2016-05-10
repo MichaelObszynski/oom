713b280f7fe7de380e7d750868aa226579e1a9a4
Michael Obszynski <michael.obszynski@gmail.com>
2016-05-10 16:30:28 +0200

    Task2 - still work to do

---
+++ tasks/Task2/Bike.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Bike
+    {
+    private string m_producer;
+        private string m_modelno;
+        private decimal m_price;
+    
+        public string Producer => m_producer;
+        public string Model => m_modelno;
+
+        public decimal Price
+        {
+            get
+            {
+                return m_price;
+            }
+            set
+            {
+                if (value <= 0) throw new Exception("Preis muss positiven Wert beinhalten!");
+                value = m_price;
+            }
+        }
+
+    }
---
+++ tasks/Task2/task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Bikeshop
+    {
+        public static int Price { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" {0} \n", Price);
+
+
+            Console.WriteLine("Hallo Freunde! \n" + (2*3) + ".");
+        }
+    }
+}
